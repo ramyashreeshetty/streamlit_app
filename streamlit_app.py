d66c13ba66348607a00fcac82331dfe3e41c3b3f
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-09-25 16:03:51 +0530

    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new health diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega3 & Blueberry meal')
+streamlit.text('Kale, Spinach & Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
