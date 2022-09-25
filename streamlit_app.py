517d1b1ac78b6565ae26525412133d884667a4f5
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-09-25 16:16:35 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title('My parents new health diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega3 & Blueberry meal')
-streamlit.text('Kale, Spinach & Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
