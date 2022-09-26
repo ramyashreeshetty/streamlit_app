e2c8fb121a60f73f9b25b4f0ad1d9da8d74e14c5
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-09-26 15:53:12 +0530

    normalized

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
 
